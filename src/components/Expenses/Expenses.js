ce5740c27e5241d72d3af669ec051598b664878c
Giorgio Cembran <giorgio.cembran@telecomitalia.it>
2022-07-15 22:51:26 +0200

    states and list filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,21 @@
 import "./Expenses.css";
+import React, { useState } from 'react';
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-    const items = props.expenses.map(element => <ExpenseItem expenses={element} />);
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const [filteredExpenses, setFilteredExpenses] = useState(props.expenses);
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+        setFilteredExpenses(props.expenses.filter(element => element.date.getFullYear().toString() === selectedYear));
+    };
+    const items = filteredExpenses.map(element => <ExpenseItem expenses={element} />);
 
     return (
         <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             {items}
         </Card>
     );
