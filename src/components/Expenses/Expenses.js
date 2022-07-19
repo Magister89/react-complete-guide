cf4567dc266ac68f85bccc45dab6e5e21f12a132
Magister89 <giorgio.cembran@gmail.com>
2022-07-19 16:52:22 +0200

    Charts and modified README

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Card from "../UI/Card";
 import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -17,6 +18,7 @@ const Expenses = (props) => {
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+            <ExpensesChart expenses={items}/>
             <ExpensesList items={items} />
         </Card>
     );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,29 @@
 import './NewExpense.css';
+import { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+    const [showForm, setShowForm] = useState(false);
+
     const saveExpenseDataHandler = (enteredExpanseData) => {
         const expenseData = {
             ...enteredExpanseData,
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        hideFormHandler();
     };
 
-    return(<div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>);
+    const showFormHandler = () => setShowForm(true);
+    const hideFormHandler = () => setShowForm(false);
+
+
+    return (
+        <div className="new-expense">
+            {!showForm && <button onClick={showFormHandler}>Add New Expense</button>}
+            {showForm && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={hideFormHandler}/>}
+        </div>
+    );
 };
 
 export default NewExpense;
